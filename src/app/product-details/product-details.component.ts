9d5e78f6db8c2e2b1af061749e23e232230f5cb1
vincen <vincen.gao@gmail.com>
2021-02-16 21:59:22 +0800

    finish chapter 2

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product:any;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
+
+    // FIXIT becase params.get() returen an union type, string | null
+    // this.route.paramMap.subscribe(params => {
+    //   this.product = products[+params.get('productId')];
+    // });
+  }
+
+}
